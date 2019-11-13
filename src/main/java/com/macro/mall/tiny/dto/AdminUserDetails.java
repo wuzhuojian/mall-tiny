a814f1874659a075f8407458d48bca0f92370cfa
wuzhuojian <zjwu920@hotmail.com>
2019-11-13 23:41:19 +0900

    警告消除

--- src/main/java/com/macro/mall/tiny/dto/AdminUserDetails.java
+++ src/main/java/com/macro/mall/tiny/dto/AdminUserDetails.java
@@ -11,53 +11,52 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * SpringSecurity需要的用户详情
- * Created by macro on 2018/4/26.
+ * SpringSecurity需要的用户详情 Created by macro on 2018/4/26.
  */
 public class AdminUserDetails implements UserDetails {
-    private UmsAdmin umsAdmin;
-    private List<UmsPermission> permissionList;
-    public AdminUserDetails(UmsAdmin umsAdmin, List<UmsPermission> permissionList) {
-        this.umsAdmin = umsAdmin;
-        this.permissionList = permissionList;
-    }
+	private static final long serialVersionUID = 1L;
+	private UmsAdmin umsAdmin;
+	private List<UmsPermission> permissionList;
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        //返回当前用户的权限
-        return permissionList.stream()
-                .filter(permission -> permission.getValue()!=null)
-                .map(permission ->new SimpleGrantedAuthority(permission.getValue()))
-                .collect(Collectors.toList());
-    }
+	public AdminUserDetails(UmsAdmin umsAdmin, List<UmsPermission> permissionList) {
+		this.umsAdmin = umsAdmin;
+		this.permissionList = permissionList;
+	}
 
-    @Override
-    public String getPassword() {
-        return umsAdmin.getPassword();
-    }
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// 返回当前用户的权限
+		return permissionList.stream().filter(permission -> permission.getValue() != null)
+				.map(permission -> new SimpleGrantedAuthority(permission.getValue())).collect(Collectors.toList());
+	}
 
-    @Override
-    public String getUsername() {
-        return umsAdmin.getUsername();
-    }
+	@Override
+	public String getPassword() {
+		return umsAdmin.getPassword();
+	}
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
+	@Override
+	public String getUsername() {
+		return umsAdmin.getUsername();
+	}
 
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
 
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
 
-    @Override
-    public boolean isEnabled() {
-        return umsAdmin.getStatus().equals(1);
-    }
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return umsAdmin.getStatus().equals(1);
+	}
 }
